d5e3ac776e9148cf8cf85fecf310749c0fc50739
bot <root@0x.mk>
2021-04-22 15:00:21 +0800

    5f7d8732f6ca293f

---
+++ asset_i_ja.db/shader.sql
@@ -0,0 +1,60 @@
+CREATE TABLE shader(
+  asset_path TEXT NOT NULL,
+  pack_name TEXT NOT NULL,
+  head INTEGER NOT NULL,
+  size INTEGER NOT NULL,
+  key1 INTEGER NOT NULL,
+  key2 INTEGER NOT NULL,
+  PRIMARY KEY (asset_path)
+);
+INSERT INTO `shader` VALUES (/*asset_path*/"§?D#", /*pack_name*/"jygnpl", /*head*/33744, /*size*/5172, /*key1*/439008494, /*key2*/-879847285);
+INSERT INTO `shader` VALUES (/*asset_path*/"§n8#", /*pack_name*/"ptqqoh", /*head*/60707, /*size*/5969, /*key1*/-1063431305, /*key2*/845335800);
+INSERT INTO `shader` VALUES (/*asset_path*/"§v;x", /*pack_name*/"ptqqoh", /*head*/66676, /*size*/6810, /*key1*/-1349862431, /*key2*/779557899);
+INSERT INTO `shader` VALUES (/*asset_path*/"§~+", /*pack_name*/"ptqqoh", /*head*/73486, /*size*/8605, /*key1*/-1398389489, /*key2*/-1892424367);
+INSERT INTO `shader` VALUES (/*asset_path*/"§%%1", /*pack_name*/"ptqqoh", /*head*/82091, /*size*/8471, /*key1*/-458751462, /*key2*/210497682);
+INSERT INTO `shader` VALUES (/*asset_path*/"§y7", /*pack_name*/"jygnpl", /*head*/72015, /*size*/6166, /*key1*/-2009030335, /*key2*/1754691476);
+INSERT INTO `shader` VALUES (/*asset_path*/"§^xP", /*pack_name*/"jygnpl", /*head*/78181, /*size*/6888, /*key1*/134435583, /*key2*/-345658264);
+INSERT INTO `shader` VALUES (/*asset_path*/"§[=$", /*pack_name*/"ptqqoh", /*head*/90562, /*size*/8102, /*key1*/442662468, /*key2*/833402882);
+INSERT INTO `shader` VALUES (/*asset_path*/"§61=", /*pack_name*/"jygnpl", /*head*/85069, /*size*/6748, /*key1*/-1594156044, /*key2*/-2096913959);
+INSERT INTO `shader` VALUES (/*asset_path*/"§VpF", /*pack_name*/"jygnpl", /*head*/91817, /*size*/7147, /*key1*/-2055326808, /*key2*/407592647);
+INSERT INTO `shader` VALUES (/*asset_path*/"§qjP", /*pack_name*/"jygnpl", /*head*/98964, /*size*/7272, /*key1*/2011603430, /*key2*/-1802856150);
+INSERT INTO `shader` VALUES (/*asset_path*/"§/#d", /*pack_name*/"ptqqoh", /*head*/98664, /*size*/7264, /*key1*/1829217839, /*key2*/-41228077);
+INSERT INTO `shader` VALUES (/*asset_path*/"§0P", /*pack_name*/"jygnpl", /*head*/106236, /*size*/6917, /*key1*/855605100, /*key2*/1741953772);
+INSERT INTO `shader` VALUES (/*asset_path*/"§7V6", /*pack_name*/"ptqqoh", /*head*/105928, /*size*/7373, /*key1*/-1593346537, /*key2*/939595247);
+INSERT INTO `shader` VALUES (/*asset_path*/"§gn", /*pack_name*/"ptqqoh", /*head*/113301, /*size*/7315, /*key1*/-384013470, /*key2*/1355030978);
+INSERT INTO `shader` VALUES (/*asset_path*/"§v(U", /*pack_name*/"jygnpl", /*head*/127753, /*size*/6082, /*key1*/1143096230, /*key2*/-492958331);
+INSERT INTO `shader` VALUES (/*asset_path*/"§ya@", /*pack_name*/"jygnpl", /*head*/133835, /*size*/6848, /*key1*/-1065768988, /*key2*/-1395927033);
+INSERT INTO `shader` VALUES (/*asset_path*/"§_3", /*pack_name*/"jygnpl", /*head*/140683, /*size*/7394, /*key1*/364182216, /*key2*/-1593833522);
+INSERT INTO `shader` VALUES (/*asset_path*/"§j^", /*pack_name*/"jygnpl", /*head*/148077, /*size*/8058, /*key1*/-733153417, /*key2*/-639888353);
+INSERT INTO `shader` VALUES (/*asset_path*/"§Vr", /*pack_name*/"ptqqoh", /*head*/120616, /*size*/8291, /*key1*/-514574873, /*key2*/2002330778);
+INSERT INTO `shader` VALUES (/*asset_path*/"§f~", /*pack_name*/"ptqqoh", /*head*/128907, /*size*/8794, /*key1*/-2124236467, /*key2*/-305190038);
+INSERT INTO `shader` VALUES (/*asset_path*/"§+~", /*pack_name*/"jygnpl", /*head*/173235, /*size*/6431, /*key1*/-2027145867, /*key2*/891430676);
+INSERT INTO `shader` VALUES (/*asset_path*/"§.5", /*pack_name*/"ptqqoh", /*head*/137701, /*size*/2678, /*key1*/764355399, /*key2*/1023334965);
+INSERT INTO `shader` VALUES (/*asset_path*/"§1!A", /*pack_name*/"1nkk4l", /*head*/45632, /*size*/1016, /*key1*/1935028340, /*key2*/1863457068);
+INSERT INTO `shader` VALUES (/*asset_path*/"§F]-", /*pack_name*/"sqv778", /*head*/0, /*size*/1051, /*key1*/-1337801950, /*key2*/-264815815);
+INSERT INTO `shader` VALUES (/*asset_path*/"§gtW", /*pack_name*/"lh493e", /*head*/0, /*size*/1027, /*key1*/-2070105112, /*key2*/686670308);
+INSERT INTO `shader` VALUES (/*asset_path*/"§3Go", /*pack_name*/"wxn2t8", /*head*/0, /*size*/1034, /*key1*/-395771712, /*key2*/827191412);
+INSERT INTO `shader` VALUES (/*asset_path*/"§D#e", /*pack_name*/"nq9i71", /*head*/0, /*size*/1032, /*key1*/1451731690, /*key2*/889939799);
+INSERT INTO `shader` VALUES (/*asset_path*/"§z]6", /*pack_name*/"gp3kx5", /*head*/0, /*size*/1025, /*key1*/-238941416, /*key2*/-834096824);
+INSERT INTO `shader` VALUES (/*asset_path*/"§;{w", /*pack_name*/"jygnpl", /*head*/182239, /*size*/6599, /*key1*/-115250990, /*key2*/2062499118);
+INSERT INTO `shader` VALUES (/*asset_path*/"§rk", /*pack_name*/"jygnpl", /*head*/188838, /*size*/6328, /*key1*/-17408894, /*key2*/1339183107);
+INSERT INTO `shader` VALUES (/*asset_path*/"§M|", /*pack_name*/"ptqqoh", /*head*/140379, /*size*/6217, /*key1*/57147323, /*key2*/655110431);
+INSERT INTO `shader` VALUES (/*asset_path*/"§R'Q", /*pack_name*/"ptqqoh", /*head*/146596, /*size*/6221, /*key1*/-922099566, /*key2*/-433325665);
+INSERT INTO `shader` VALUES (/*asset_path*/"§de", /*pack_name*/"ptqqoh", /*head*/152817, /*size*/1205, /*key1*/915273019, /*key2*/-347817354);
+INSERT INTO `shader` VALUES (/*asset_path*/"§Vf", /*pack_name*/"jygnpl", /*head*/208871, /*size*/10415, /*key1*/1944496603, /*key2*/-589811799);
+INSERT INTO `shader` VALUES (/*asset_path*/"§?{:", /*pack_name*/"jygnpl", /*head*/219286, /*size*/6045, /*key1*/898560553, /*key2*/2047045832);
+INSERT INTO `shader` VALUES (/*asset_path*/"§2/#", /*pack_name*/"ptqqoh", /*head*/154022, /*size*/8918, /*key1*/-950717235, /*key2*/-1775299473);
+INSERT INTO `shader` VALUES (/*asset_path*/"§8X8", /*pack_name*/"wpguf9", /*head*/732162, /*size*/7393, /*key1*/1396252400, /*key2*/-509937957);
+INSERT INTO `shader` VALUES (/*asset_path*/"§r?", /*pack_name*/"jygnpl", /*head*/232724, /*size*/8050, /*key1*/-668506479, /*key2*/-691701050);
+INSERT INTO `shader` VALUES (/*asset_path*/"§`s@", /*pack_name*/"1nkk4l", /*head*/46648, /*size*/7830, /*key1*/2146530823, /*key2*/-2025518950);
+INSERT INTO `shader` VALUES (/*asset_path*/"§OdT", /*pack_name*/"sqv778", /*head*/1051, /*size*/7717, /*key1*/2135559680, /*key2*/-86607235);
+INSERT INTO `shader` VALUES (/*asset_path*/"§P!g", /*pack_name*/"lh493e", /*head*/1027, /*size*/7051, /*key1*/-161519636, /*key2*/-90607557);
+INSERT INTO `shader` VALUES (/*asset_path*/"§pnO", /*pack_name*/"wxn2t8", /*head*/1034, /*size*/7959, /*key1*/526191856, /*key2*/-1808741242);
+INSERT INTO `shader` VALUES (/*asset_path*/"§G]_", /*pack_name*/"nq9i71", /*head*/1032, /*size*/7948, /*key1*/291565415, /*key2*/-1858716481);
+INSERT INTO `shader` VALUES (/*asset_path*/"§7X@", /*pack_name*/"gp3kx5", /*head*/1025, /*size*/7047, /*key1*/-1754948042, /*key2*/-1197882513);
+INSERT INTO `shader` VALUES (/*asset_path*/"§9%>", /*pack_name*/"jygnpl", /*head*/240774, /*size*/6208, /*key1*/-505121944, /*key2*/873581103);
+INSERT INTO `shader` VALUES (/*asset_path*/"§fR=", /*pack_name*/"jygnpl", /*head*/246982, /*size*/7089, /*key1*/-1272792274, /*key2*/1664023055);
+INSERT INTO `shader` VALUES (/*asset_path*/"§{;", /*pack_name*/"jygnpl", /*head*/254071, /*size*/236595, /*key1*/-308815141, /*key2*/-1492776324);
+INSERT INTO `shader` VALUES (/*asset_path*/"§4`.", /*pack_name*/"jygnpl", /*head*/490666, /*size*/7659, /*key1*/-1668787429, /*key2*/-2107502029);
+INSERT INTO `shader` VALUES (/*asset_path*/"§E\"~", /*pack_name*/"ptqqoh", /*head*/162940, /*size*/7572, /*key1*/-19320042, /*key2*/-1554679603);
+INSERT INTO `shader` VALUES (/*asset_path*/"§<=", /*pack_name*/"jygnpl", /*head*/498325, /*size*/6627, /*key1*/975371757, /*key2*/907344252);
---
+++ masterdata.db/m_member_guild_period.sql
@@ -0,0 +1,12 @@
+CREATE TABLE m_member_guild_period(
+  id INTEGER NOT NULL,
+  start_at INTEGER NOT NULL,
+  end_at INTEGER,
+  transfer_start_secs INTEGER NOT NULL,
+  transfer_end_secs INTEGER NOT NULL,
+  ranking_start_secs INTEGER NOT NULL,
+  ranking_end_secs INTEGER NOT NULL,
+  one_cycle_secs INTEGER NOT NULL,
+  PRIMARY KEY (id)
+);
+INSERT INTO `m_member_guild_period` VALUES (/*id*/1, /*start_at*/1617008400, /*end_at*/"", /*transfer_start_secs*/0, /*transfer_end_secs*/151200, /*ranking_start_secs*/151200, /*ranking_end_secs*/583200, /*one_cycle_secs*/604800);
