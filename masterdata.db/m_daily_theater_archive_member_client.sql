39cc7d9f900c586876b79b517c93d57a09508d87
bot <root@0x.mk>
2021-10-11 15:00:18 +0800

    0b514348f554ef85

--- masterdata.db/m_daily_theater_archive_member_client.sql
+++ masterdata.db/m_daily_theater_archive_member_client.sql
@@ -1324,3 +1324,7 @@ INSERT INTO `m_daily_theater_archive_member_client` VALUES (/*daily_theater_id*/
 INSERT INTO `m_daily_theater_archive_member_client` VALUES (/*daily_theater_id*/1000615, /*member_master_id*/206);
 INSERT INTO `m_daily_theater_archive_member_client` VALUES (/*daily_theater_id*/1000615, /*member_master_id*/208);
 INSERT INTO `m_daily_theater_archive_member_client` VALUES (/*daily_theater_id*/1000615, /*member_master_id*/209);
+INSERT INTO `m_daily_theater_archive_member_client` VALUES (/*daily_theater_id*/1000616, /*member_master_id*/9);
+INSERT INTO `m_daily_theater_archive_member_client` VALUES (/*daily_theater_id*/1000616, /*member_master_id*/106);
+INSERT INTO `m_daily_theater_archive_member_client` VALUES (/*daily_theater_id*/1000617, /*member_master_id*/4);
+INSERT INTO `m_daily_theater_archive_member_client` VALUES (/*daily_theater_id*/1000617, /*member_master_id*/205);
