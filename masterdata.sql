e763861e0d7b2d1483088e60a726f97506aa44b3
bot <root@0x.mk>
2022-01-26 17:00:30 +0800

    b51202661482ea21

--- masterdata.sql
+++ masterdata.sql
@@ -1,5 +1,5 @@
 CREATE TABLE masterdata(name TEXT NOT NULL, keys TEXT NOT NULL, hash TEXT NOT NULL, size INTEGER NOT NULL);
-INSERT INTO `masterdata` VALUES (/*name*/"masterdata.db", /*keys*/"b6f5a110bc290a3afe49bb866ccb3ad85a6c1143", /*hash*/"d1fa3b17d095429b0a29158470ee4d4465daf4f7", /*size*/9878423);
+INSERT INTO `masterdata` VALUES (/*name*/"masterdata.db", /*keys*/"e3ad49f5768bcf68b9ce6dc63835172b2f05480a", /*hash*/"9306066d7e9db2ef1109b3fedf585e1accc1e8a8", /*size*/9878423);
 INSERT INTO `masterdata` VALUES (/*name*/"dictionary_ja_m.db", /*keys*/"c999234871a39582d6246bd24b1e55afd60f21eb", /*hash*/"6ecf37fbc16f893049883c1e0f4f964d66df26e5", /*size*/183386);
 INSERT INTO `masterdata` VALUES (/*name*/"dictionary_ja_ios.db", /*keys*/"692f69ffd025390b13a81619950e36d34dcfaf2d", /*hash*/"9bf7f56a57137d84ac647cdb3ad6ea1c3ce603fb", /*size*/38277);
 INSERT INTO `masterdata` VALUES (/*name*/"dictionary_ja_inline_image.db", /*keys*/"23ffc7eebdf70da52e53c3068726a42e698a80e6", /*hash*/"ff093874be8046fd7d28a31133e427a8deec7d2c", /*size*/729);
@@ -8,4 +8,4 @@ INSERT INTO `masterdata` VALUES (/*name*/"dictionary_ja_v.db", /*keys*/"caec73a4
 INSERT INTO `masterdata` VALUES (/*name*/"dictionary_ja_dummy.db", /*keys*/"efbafaf8e51ccac23a5a81d581bbe05b11410c34", /*hash*/"148945e69e24690edd3b8d5b3954391a4184b393", /*size*/6608);
 INSERT INTO `masterdata` VALUES (/*name*/"dictionary_ja_s.db", /*keys*/"c7635d888da2a5dfb673a2c36af9e75921ab5c5c", /*hash*/"2ddbe78c2a05830d2e98ae390cefe25991c310c2", /*size*/84998);
 INSERT INTO `masterdata` VALUES (/*name*/"dictionary_ja_k.db", /*keys*/"9982a2f7bcad46b4446f591bc921107bbfa1cfb1", /*hash*/"53666077c80778d27d6e7c3dae010c30175c039e", /*size*/1440566);
-INSERT INTO `masterdata` VALUES (/*name*/"asset_i_ja.db", /*keys*/"c50ae961ffd7fb6050d09585f25f170be2ca0db2", /*hash*/"69be4eab021ae8b63dc33711b4f61ee10234bce7", /*size*/12553043);
+INSERT INTO `masterdata` VALUES (/*name*/"asset_i_ja.db", /*keys*/"c50ae961ffd7fb6050d09585f25f170be2ca0db2", /*hash*/"2dec5bd534f1e7a20b5c3a7db23bce68fa87431a", /*size*/12550955);
